58cfc208c64d28943416340dc7084f9529bbfb6d
Inês Almeida <ines_almeida_203@hotmail.com>
2023-07-26 16:42:21 +0100

    done

--- src/App.js
+++ src/App.js
@@ -7,21 +7,54 @@ import './App.css';
 function App() {
   const [contactsList, setContactsList] = useState(contacts.slice(0, 5));
 
-//create function to add a new random contact for every click
-const addContact = () => {
-  //check all the remaining contact
-  //contacts different from the contactList
-  //if the contact doesn't belong to the contact list add it 
-  //newContact.push
-  //update the state with the new contact list
-  //setContactsList(contacts + new contact)
-}
+  const addContact = () => {
+    const remainingContacts = contacts.filter(
+      contact => !contactsList.some(existingContact => existingContact.id === contact.id)
+    );
+  
+    if (remainingContacts.length > 0) {
+      const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+      const randomContact = remainingContacts[randomIndex];
+  
+      const updatedContactsList = [...contactsList, randomContact];
+  
+      setContactsList(updatedContactsList);
+    }
+  };
+
+  const sortByPopularity = () => {
+    const popularitySort = [...contactsList].sort((a,b) => b.popularity - a.popularity);
+
+    setContactsList(popularitySort)
+  }
+
+  const sortByName = () => {
+    const nameSort = [...contactsList].sort(function(a,b) {
+      if(a.name < b.name) { 
+        return -1
+      };
+      if(a.name > b.name) {
+      return 1 
+    };
+      return 0
+    })
+    
+    setContactsList(nameSort)
+  }
+
+  const deleteContact = (id) => {
+    const updatedContactsList = contactsList.filter((contact) => contact.id !== id);
+  
+    setContactsList(updatedContactsList);
+  };
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
 
-      <button onClick={ addContact }>Add Random Contact</button>
+      <button onClick={addContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
 
       <table>
         <thead>
@@ -31,6 +64,7 @@ const addContact = () => {
             <th>Popularity</th>
             <th>Won Oscar</th>
             <th>Won Emmy</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -41,6 +75,7 @@ const addContact = () => {
               <td>{contact.popularity.toFixed(2)}</td>
               <td>{contact.wonOscar && <img className="award-img" src={Oscar} alt='Oscar'/>}</td>
               <td>{contact.wonEmmy && <img className="award-img" src={Emmy} alt='Emmy'/>}</td>
+              <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
